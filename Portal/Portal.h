a70d53a41c1e80e6aaa9c1d13852d5d35c3bd861
Paul Young <paulyoungonline@gmail.com>
2016-01-06 12:32:28 -0800

    Add Mac targets.

---
+++ Portal/Portal.h
@@ -0,0 +1,19 @@
+//
+//  Portal.h
+//  Portal
+//
+//  Created by Paul Young on 1/6/16.
+//  Copyright © 2016 The Grid. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Portal.
+FOUNDATION_EXPORT double PortalVersionNumber;
+
+//! Project version string for Portal.
+FOUNDATION_EXPORT const unsigned char PortalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Portal/PublicHeader.h>
+
+
