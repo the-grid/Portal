a95a5369b0e6a230776d2fbca5659e5bcf1740d8
Paul Young <paulyoungonline@gmail.com>
2016-01-13 14:19:42 -0800

    Add extension to Manager for setting Authorization header.
    *
    https://github.com/Alamofire/Alamofire/issues/545#issuecomment-171422719

--- Portal/Networking/APIClient.swift
+++ Portal/Networking/APIClient.swift
@@ -5,11 +5,10 @@ let baseUrl = "https://api.thegrid.io"
 /// An API client for The Grid.
 final public class APIClient {
     let manager: Manager
+    let token: String
     
     public init(token: String, configuration: NSURLSessionConfiguration = .defaultSessionConfiguration()) {
-        let config = configuration.copy() as! NSURLSessionConfiguration
-        config.HTTPAdditionalHeaders = config.HTTPAdditionalHeaders ?? [:]
-        config.HTTPAdditionalHeaders?.updateValue("Bearer \(token)", forKey: "Authorization")
-        self.manager = Manager.init(configuration: config)
+        self.manager = Manager(configuration: configuration)
+        self.token = token
     }
 }
---
+++ Portal/Networking/Extensions/Manager.swift
@@ -0,0 +1,16 @@
+import Alamofire
+
+extension Manager {
+    func request(
+        method: Alamofire.Method,
+        _ URLString: URLStringConvertible,
+        parameters: [String: AnyObject]? = nil,
+        encoding: ParameterEncoding = .URL,
+        headers: [String: String]? = nil,
+        token: String
+    ) -> Request {
+        var requestHeaders = headers ?? [:]
+        requestHeaders["Authorization"] = "Bearer \(token)"
+        return request(method, URLString, parameters: parameters, encoding: encoding, headers: requestHeaders)
+    }
+}
