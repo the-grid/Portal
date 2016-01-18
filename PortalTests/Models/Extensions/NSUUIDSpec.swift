c8ed9dbfb2ad1b984a4913fb58ec7bb22c1b7129
Paul Young <paulyoungonline@gmail.com>
2016-01-18 14:49:59 -0800

    Add spec for extension to NSUUID.

---
+++ PortalTests/Models/Extensions/NSUUIDSpec.swift
@@ -0,0 +1,30 @@
+import Argo
+import Foundation
+import Nimble
+import Ogra
+import Portal
+import Quick
+
+class NSUUIDSpec: QuickSpec {
+    override func spec() {
+        let uuidString = "f36401b6-39ff-4a72-9999-12a0abe709a9"
+        let json: JSON = .String(uuidString)
+        let uuid = NSUUID.init(UUIDString: uuidString)
+        
+        describe("decoding") {
+            it("should produce an NSUUID") {
+                guard let decoded = NSUUID.decode(json).value else {
+                    return XCTFail("Unable to decode JSON: \(json)")
+                }
+                expect(decoded).to(equal(uuid))
+            }
+        }
+        
+        describe("encoding") {
+            it("should produce JSON") {
+                let encoded = uuid.encode()
+                expect(encoded).to(equal(json))
+            }
+        }
+    }
+}
