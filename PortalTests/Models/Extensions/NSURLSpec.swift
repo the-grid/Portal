f243b39959b0813b974313c997cbf06fa2eedcb7
Paul Young <paulyoungonline@gmail.com>
2016-01-18 14:49:23 -0800

    Add spec for extension to NSURL.

---
+++ PortalTests/Models/Extensions/NSURLSpec.swift
@@ -0,0 +1,30 @@
+import Argo
+import Foundation
+import Nimble
+import Ogra
+import Portal
+import Quick
+
+class NSURLSpec: QuickSpec {
+    override func spec() {
+        let urlString = "https://thegrid.io"
+        let json: JSON = .String(urlString)
+        let url = NSURL(string: urlString)
+        
+        describe("decoding") {
+            it("should produce an NSURL") {
+                guard let decoded = NSURL.decode(json).value else {
+                    return XCTFail("Unable to decode JSON: \(json)")
+                }
+                expect(decoded).to(equal(url))
+            }
+        }
+        
+        describe("encoding") {
+            it("should produce JSON") {
+                let encoded = url.encode()
+                expect(encoded).to(equal(json))
+            }
+        }
+    }
+}
