f2f74a67baa543cf53dfe94e72f653ea5e81df2d
Paul Young <paulyoungonline@gmail.com>
2016-01-23 16:38:58 -0800

    Add spec and implementation for Author model.

---
+++ Portal/Models/Author.swift
@@ -0,0 +1,63 @@
+import Argo
+import Curry
+import Foundation
+import Ogra
+
+/// An author.
+public struct Author {
+    public let avatar: Avatar?
+    public let email: String?
+    public let name: String?
+    public let url: NSURL?
+    
+    public init(
+        avatar: Avatar? = .None,
+        email: String? = .None,
+        name: String? = .None,
+        url: NSURL? = .None
+    ) {
+        self.avatar = avatar
+        self.email = email
+        self.name = name
+        self.url = url
+    }
+}
+
+
+// MARK: - Decodable
+
+extension Author: Decodable {
+    public static func decode(json: JSON) -> Decoded<Author> {
+        return curry(self.init)
+            <^> json <|? "avatar"
+            <*> json <|? "email"
+            <*> json <|? "name"
+            <*> json <|? "url"
+    }
+}
+
+
+// MARK: - Encodable
+
+extension Author: Encodable {
+    public func encode() -> JSON {
+        return .Object([
+            "avatar": avatar.encode(),
+            "email": email.encode(),
+            "name": name.encode(),
+            "url": url.encode()
+        ])
+    }
+}
+
+
+// MARK: - Equatable
+
+extension Author: Equatable {}
+
+public func == (lhs: Author, rhs: Author) -> Bool {
+    return lhs.avatar == rhs.avatar
+        && lhs.email == rhs.email
+        && lhs.name == rhs.name
+        && lhs.url == rhs.url
+}
---
+++ PortalTests/Models/AuthorSpec.swift
@@ -0,0 +1,77 @@
+import Argo
+import Nimble
+import Ogra
+import Portal
+import Quick
+
+class AuthorSpec: QuickSpec {
+    override func spec() {
+        let red = 64
+        let green = 64
+        let blue = 64
+        
+        let colors = [
+            [ red, green, blue ]
+        ]
+        
+        let height = 80
+        let avatarUrl = "https://s.gravatar.com/avatar/6a12a226806bb77aa45dac4133d76227"
+        let width = 80
+        
+        let colorsJson: JSON = .Array(colors.map { rgb in
+            .Array(rgb.map { number in
+                .Number(number)
+            })
+        })
+        
+        let avatarJson: JSON = .Object([
+            "colors": colorsJson,
+            "height": .Number(height),
+            "src": .String(avatarUrl),
+            "width": .Number(width)
+        ])
+        
+        let avatar = Avatar(
+            colors: [
+                Color(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
+            ],
+            height: height,
+            url: NSURL(string: avatarUrl),
+            width: width
+        )
+
+        let email = "bear@thegrid.io"
+        let name = "Grid Bear"
+        let url = "https://twitter.com/grid_bear"
+        
+        let json: JSON = .Object([
+            "avatar": avatarJson,
+            "email": .String(email),
+            "name": .String(name),
+            "url": .String(url)
+        ])
+        
+        let author = Author(
+            avatar: avatar,
+            email: email,
+            name: name,
+            url: NSURL(string: url)
+        )
+        
+        describe("decoding") {
+            it("should produce a Author") {
+                guard let decoded = Author.decode(json).value else {
+                    return XCTFail("Unable to decode JSON: \(json)")
+                }
+                expect(decoded).to(equal(author))
+            }
+        }
+        
+        describe("encoding") {
+            it("should produce JSON") {
+                let encoded = author.encode()
+                expect(encoded).to(equal(json))
+            }
+        }
+    }
+}
