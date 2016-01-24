cf5b701b31eef26f62d418ea0d10f5a2a2987fc8
Paul Young <paulyoungonline@gmail.com>
2016-01-23 17:36:49 -0800

    Add spec and implementation for Publisher model.

---
+++ Portal/Models/Publisher.swift
@@ -0,0 +1,63 @@
+import Argo
+import Curry
+import Foundation
+import Ogra
+
+/// A publisher.
+public struct Publisher {
+    let domain: String?
+    let favicon: NSURL?
+    let name: String?
+    let url: NSURL?
+    
+    public init(
+        domain: String? = .None,
+        favicon: NSURL? = .None,
+        name: String? = .None,
+        url: NSURL? = .None
+    ) {
+        self.domain = domain
+        self.favicon = favicon
+        self.name = name
+        self.url = url
+    }
+}
+
+
+// MARK: - Decodable
+
+extension Publisher: Decodable {
+    public static func decode(json: JSON) -> Decoded<Publisher> {
+        return curry(self.init)
+            <^> json <|? "domain"
+            <*> json <|? "favicon"
+            <*> json <|? "name"
+            <*> json <|? "url"
+    }
+}
+
+
+// MARK: - Encodable
+
+extension Publisher: Encodable {
+    public func encode() -> JSON {
+        return .Object([
+            "domain": domain.encode(),
+            "favicon": favicon.encode(),
+            "name": name.encode(),
+            "url": url.encode()
+        ])
+    }
+}
+
+
+// MARK: - Equatable
+
+extension Publisher: Equatable {}
+
+public func == (lhs: Publisher, rhs: Publisher) -> Bool {
+    return lhs.domain == rhs.domain
+        && lhs.favicon == rhs.favicon
+        && lhs.name == rhs.name
+        && lhs.url == rhs.url
+}
---
+++ PortalTests/Models/PublisherSpec.swift
@@ -0,0 +1,44 @@
+import Argo
+import Nimble
+import Ogra
+import Portal
+import Quick
+
+class PublisherSpec: QuickSpec {
+    override func spec() {
+        let domain = "gridbear.com"
+        let favicon = "https://gridbear.com"
+        let name = "Grid Bear"
+        let url = "https://gridbear.com/favicon.png"
+        
+        let json: JSON = .Object([
+            "domain": .String(domain),
+            "favicon": .String(favicon),
+            "name": .String(name),
+            "url": .String(url)
+        ])
+        
+        let publisher = Publisher(
+            domain: domain,
+            favicon: NSURL(string: favicon),
+            name: name,
+            url: NSURL(string: url)
+        )
+        
+        describe("decoding") {
+            it("should produce a Publisher") {
+                guard let decoded = Publisher.decode(json).value else {
+                    return XCTFail("Unable to decode JSON: \(json)")
+                }
+                expect(decoded).to(equal(publisher))
+            }
+        }
+        
+        describe("encoding") {
+            it("should produce JSON") {
+                let encoded = publisher.encode()
+                expect(encoded).to(equal(json))
+            }
+        }
+    }
+}
