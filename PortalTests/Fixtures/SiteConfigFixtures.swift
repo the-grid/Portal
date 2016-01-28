a3ce92e34e77f78326953a00caf51d009d69573c
Paul Young <paulyoungonline@gmail.com>
2016-01-27 23:40:59 -0800

    Create SiteConfig fixtures.

---
+++ PortalTests/Fixtures/SiteConfigFixtures.swift
@@ -0,0 +1,32 @@
+import Argo
+import Foundation
+import Portal
+
+private let favicon = "https://gridbear.com/favicon.png"
+private let layout: Float = 0.5
+private let logo = "https://gridbear.com/logo.png"
+private let typography: Float = 0.5
+
+let siteConfigResponseBody: [String: AnyObject] = [
+    "color": colorConfigResponseBody,
+    "favicon": favicon,
+    "layout_spectrum": layout,
+    "logo": logo,
+    "typography_spectrum": typography
+]
+
+let siteConfigJson: JSON = .Object([
+    "color": colorConfigJson,
+    "favicon": .String(favicon),
+    "layout_spectrum": .Number(layout),
+    "logo": .String(logo),
+    "typography_spectrum": .Number(typography)
+])
+
+let siteConfigModel = SiteConfig(
+    color: colorConfigModel,
+    favicon: NSURL(string: favicon),
+    layout: layout,
+    logo: NSURL(string: logo),
+    typography: typography
+)
--- PortalTests/Models/SiteConfigSpec.swift
+++ PortalTests/Models/SiteConfigSpec.swift
@@ -8,60 +8,19 @@ import XCTest
 
 class SiteConfigSpec: QuickSpec {
     override func spec() {
-        let brandColors = [ "#000000", "#ffffff" ]
-        let brandStrength: Float = 0.5
-        let lightness: Float = 0.5
-        let saturation: Float = 0.5
-        
-        let favicon = "https://gridbear.com/favicon.png"
-        let layout: Float = 0.5
-        let logo = "https://gridbear.com/logo.png"
-        let typography: Float = 0.5
-        
-        let json: JSON = .Object([
-            "color": .Object([
-                "brandColors": .Array(brandColors.map(JSON.String)),
-                "brandStrength": .Number(brandStrength),
-                "lightness": .Number(lightness),
-                "saturation": .Number(saturation)
-            ]),
-            "favicon": .String(favicon),
-            "layout_spectrum": .Number(layout),
-            "logo": .String(logo),
-            "typography_spectrum": .Number(typography)
-        ])
-        
-        let colorConfig = ColorConfig(
-            brandColors: [
-                Color(red: 0, green: 0, blue: 0, alpha: 1),
-                Color(red: 1, green: 1, blue: 1, alpha: 1)
-            ],
-            brandStrength: brandStrength,
-            lightness: lightness,
-            saturation: saturation
-        )
-        
-        let siteConfig = SiteConfig(
-            color: colorConfig,
-            favicon: NSURL(string: favicon),
-            layout: layout,
-            logo: NSURL(string: logo),
-            typography: typography
-        )
-        
         describe("decoding") {
             it("should produce a SiteConfig") {
-                guard let decoded = SiteConfig.decode(json).value else {
-                    return XCTFail("Unable to decode JSON: \(json)")
+                guard let decoded = SiteConfig.decode(siteConfigJson).value else {
+                    return XCTFail("Unable to decode JSON: \(siteConfigJson)")
                 }
-                expect(decoded).to(equal(siteConfig))
+                expect(decoded).to(equal(siteConfigModel))
             }
         }
         
         describe("encoding") {
             it("should produce JSON") {
-                let encoded = siteConfig.encode()
-                expect(encoded).to(equal(json))
+                let encoded = siteConfigModel.encode()
+                expect(encoded).to(equal(siteConfigJson))
             }
         }
     }
