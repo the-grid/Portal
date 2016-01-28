c268162a0b8454d867344aa07bd0be055c7bfbc1
Paul Young <paulyoungonline@gmail.com>
2016-01-27 23:04:30 -0800

    Create Metadata fixtures.

---
+++ PortalTests/Fixtures/MetadataFixtures.swift
@@ -0,0 +1,40 @@
+import Argo
+import Foundation
+import Portal
+
+private let basedOnUrl = "https://gridbear.com"
+private let keywords = [ "grid", "bear" ]
+private let starred = true
+
+let metadataResponseBody: [String: AnyObject] = [
+    "authors": [ authorResponseBody ],
+    "isBasedOnUrl": basedOnUrl,
+    "coverPrefs": coverPreferencesResponseBody,
+    "keywords": keywords,
+    "inLanguage": languageResponseBody,
+    "publisher": publisherResponseBody,
+    "starred": starred,
+    "@type": schemaTypeResponseBody
+]
+
+let metadataJson: JSON = .Object([
+    "authors": .Array([ authorJson ]),
+    "isBasedOnUrl": .String(basedOnUrl),
+    "coverPrefs": coverPreferencesJson,
+    "keywords": .Array(keywords.map(JSON.String)),
+    "inLanguage": languageJson,
+    "publisher": publisherJson,
+    "starred": .Number(starred),
+    "@type": schemaTypeJson
+])
+
+let metadataModel = Metadata(
+    authors: [ authorModel ],
+    basedOnUrl: NSURL(string: basedOnUrl),
+    coverPreferences: coverPreferencesModel,
+    keywords: keywords,
+    language: languageModel,
+    publisher: publisherModel,
+    starred: starred,
+    type: schemaTypeModel
+)
--- PortalTests/Models/MetadataSpec.swift
+++ PortalTests/Models/MetadataSpec.swift
@@ -8,148 +8,19 @@ import XCTest
 
 class MetadataSpec: QuickSpec {
     override func spec() {
-        let red = 64
-        let green = 64
-        let blue = 64
-        
-        let avatarColors = [
-            [ red, green, blue ]
-        ]
-        
-        let avatarHeight = 80
-        let avatarUrl = "https://s.gravatar.com/avatar/6a12a226806bb77aa45dac4133d76227"
-        let avatarWidth = 80
-        
-        let colorsJson: JSON = .Array(avatarColors.map { rgb in
-            .Array(rgb.map { number in
-                .Number(number)
-                })
-            })
-        
-        let avatarJson: JSON = .Object([
-            "colors": colorsJson,
-            "height": .Number(avatarHeight),
-            "src": .String(avatarUrl),
-            "width": .Number(avatarWidth)
-        ])
-        
-        let avatar = Avatar(
-            colors: [
-                Color(red: CGFloat(red) / 255, green: CGFloat(green) / 255, blue: CGFloat(blue) / 255, alpha: 1)
-            ],
-            height: avatarHeight,
-            url: NSURL(string: avatarUrl),
-            width: avatarWidth
-        )
-        
-        
-        let authorEmail = "bear@thegrid.io"
-        let authorName = "Grid Bear"
-        let authorUrl = "https://twitter.com/grid_bear"
-        
-        let authorJson: JSON = .Object([
-            "avatar": avatarJson,
-            "email": .String(authorEmail),
-            "name": .String(authorName),
-            "url": .String(authorUrl)
-        ])
-        
-        let author = Author(
-            avatar: avatar,
-            email: authorEmail,
-            name: authorName,
-            url: NSURL(string: authorUrl)
-        )
-        
-        let authors = [ author ]
-        
-        
-        let basedOnUrl = "https://gridbear.com"
-        
-        
-        let crop = true
-        let filter = true
-        let overlay = true
-        
-        let coverPreferencesJson: JSON = .Object([
-            "crop": .Number(crop),
-            "filter": .Number(filter),
-            "overlay": .Number(overlay)
-        ])
-        
-        let coverPreferences = CoverPreferences(
-            crop: crop,
-            filter: filter,
-            overlay: overlay
-        )
-        
-        
-        let keywords = [ "grid", "bear" ]
-        
-        
-        let language = "en"
-        
-        
-        let publisherDomain = "gridbear.com"
-        let publisherFavicon = "https://gridbear.com"
-        let publisherName = "Grid Bear"
-        let publisherUrl = "https://gridbear.com/favicon.png"
-        
-        let publisherJson: JSON = .Object([
-            "domain": .String(publisherDomain),
-            "favicon": .String(publisherFavicon),
-            "name": .String(publisherName),
-            "url": .String(publisherUrl)
-        ])
-        
-        let publisher = Publisher(
-            domain: publisherDomain,
-            favicon: NSURL(string: publisherFavicon),
-            name: publisherName,
-            url: NSURL(string: publisherUrl)
-        )
-        
-        
-        let starred = true
-        
-        
-        let type = "Article"
-        
-        let json: JSON = .Object([
-            "authors": .Array([ authorJson ]),
-            "isBasedOnUrl": .String(basedOnUrl),
-            "coverPrefs": coverPreferencesJson,
-            "keywords": .Array(keywords.map(JSON.String)),
-            "inLanguage": .String(language),
-            "publisher": publisherJson,
-            "starred": .Number(starred),
-            "@type": .String(type)
-        ])
-        
-        let metadata = Metadata(
-            authors: authors,
-            basedOnUrl: NSURL(string: basedOnUrl),
-            coverPreferences: coverPreferences,
-            keywords: keywords,
-            language: Language(rawValue: language),
-            publisher: publisher,
-            starred: starred,
-            type: SchemaType(rawValue: type)
-        )
-        
         describe("decoding") {
             it("should produce Metadata") {
-                guard let decoded = Metadata.decode(json).value else {
-                    return XCTFail("Unable to decode JSON: \(json)")
+                guard let decoded = Metadata.decode(metadataJson).value else {
+                    return XCTFail("Unable to decode JSON: \(metadataJson)")
                 }
-                expect(decoded).to(equal(metadata))
+                expect(decoded).to(equal(metadataModel))
             }
         }
         
         describe("encoding") {
             it("should produce JSON") {
-                let encoded = metadata.encode()
-                expect(encoded).to(equal(json))
+                let encoded = metadataModel.encode()
+                expect(encoded).to(equal(metadataJson))
             }
         }
     }
