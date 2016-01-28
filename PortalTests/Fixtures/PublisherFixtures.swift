db201703e74b89be41b26ff9ce4a8ccfe2ca9e76
Paul Young <paulyoungonline@gmail.com>
2016-01-27 22:53:19 -0800

    Create Publisher fixtures.

---
+++ PortalTests/Fixtures/PublisherFixtures.swift
@@ -0,0 +1,29 @@
+import Argo
+import Foundation
+import Portal
+
+private let domain = "gridbear.com"
+private let favicon = "https://gridbear.com"
+private let name = "Grid Bear"
+private let url = "https://gridbear.com/favicon.png"
+
+let publisherResponseBody: [String: AnyObject] = [
+    "domain": domain,
+    "favicon": favicon,
+    "name": name,
+    "url": url
+]
+
+let publisherJson: JSON = .Object([
+    "domain": .String(domain),
+    "favicon": .String(favicon),
+    "name": .String(name),
+    "url": .String(url)
+])
+
+let publisherModel = Publisher(
+    domain: domain,
+    favicon: NSURL(string: favicon),
+    name: name,
+    url: NSURL(string: url)
+)
--- PortalTests/Models/PublisherSpec.swift
+++ PortalTests/Models/PublisherSpec.swift
@@ -8,38 +8,19 @@ import XCTest
 
 class PublisherSpec: QuickSpec {
     override func spec() {
-        let domain = "gridbear.com"
-        let favicon = "https://gridbear.com"
-        let name = "Grid Bear"
-        let url = "https://gridbear.com/favicon.png"
-        
-        let json: JSON = .Object([
-            "domain": .String(domain),
-            "favicon": .String(favicon),
-            "name": .String(name),
-            "url": .String(url)
-        ])
-        
-        let publisher = Publisher(
-            domain: domain,
-            favicon: NSURL(string: favicon),
-            name: name,
-            url: NSURL(string: url)
-        )
-        
         describe("decoding") {
             it("should produce a Publisher") {
-                guard let decoded = Publisher.decode(json).value else {
-                    return XCTFail("Unable to decode JSON: \(json)")
+                guard let decoded = Publisher.decode(publisherJson).value else {
+                    return XCTFail("Unable to decode JSON: \(publisherJson)")
                 }
-                expect(decoded).to(equal(publisher))
+                expect(decoded).to(equal(publisherModel))
             }
         }
         
         describe("encoding") {
             it("should produce JSON") {
-                let encoded = publisher.encode()
-                expect(encoded).to(equal(json))
+                let encoded = publisherModel.encode()
+                expect(encoded).to(equal(publisherJson))
             }
         }
     }
