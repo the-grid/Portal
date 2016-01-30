3cef8bc873b47b6ea216fda80dc8538db63ff02a
Paul Young <paulyoungonline@gmail.com>
2016-01-30 01:05:28 -0800

    Rename variables.

--- PortalTests/Fixtures/ColorFixtures.swift
+++ PortalTests/Fixtures/ColorFixtures.swift
@@ -7,18 +7,18 @@ import Portal
     import AppKit
 #endif
 
-private let r: Float = 64
-private let g: Float = 64
-private let b: Float = 64
+private let redValue: Float = 64
+private let greenValue: Float = 64
+private let blueValue: Float = 64
 
 let colorHexResponseBody = "#404040"
-let colorRgbResponseBody = [ r, g, b ]
+let colorRgbResponseBody = [ redValue, greenValue, blueValue ]
 
 let colorHexJson: JSON = .String(colorHexResponseBody)
 let colorRgbJson: JSON = .Array(colorRgbResponseBody.map { JSON.Number($0) })
 
-private let red: CGFloat = CGFloat(r) / 255
-private let green: CGFloat = CGFloat(g) / 255
-private let blue: CGFloat = CGFloat(b) / 255
+private let red: CGFloat = CGFloat(redValue) / 255
+private let green: CGFloat = CGFloat(greenValue) / 255
+private let blue: CGFloat = CGFloat(blueValue) / 255
 
 let colorModel = Color(red: red, green: green, blue: blue, alpha: 1)
